9724c6b47993aeefbf29c6242ade78d80f47e5be
Alexander Ynoñan <alex@soho.com.uy>
2023-01-19 11:57:05 -0500

    create proyect

---
+++ WorldCup/WorldCup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldCup
+//
+//  Created by Alexander Ynoñan H. on 19/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
